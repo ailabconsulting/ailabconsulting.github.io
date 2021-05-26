1899fb011ec04b1540fda2be13e0597e2fde0c6b
Shmoilova, Natalia <natalia.shmoilova@sap.com>
2021-05-26 16:33:22 -0700

    adding js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,19 @@ const path = require('path');
 
 module.exports = {
   entry: {
+    head: './assets/js/head.js',
+    main: './assets/js/index.js',
+    collapse: './assets/js/collapse.js',
+    contact: './assets/js/contact.js',
+    content: './assets/js/content.js',
+    graph: './assets/js/graph.js',
+    hero: './assets/js/hero.js',
+    portfolio: './assets/js/portfolio.js',
+    pricing: './assets/js/pricing.js',
+    react: './assets/js/react.js',
+    search: './assets/js/search.js',
+    stripe: './assets/js/stripe.js',
+    //'dev-jq-helpers': './assets/js/helpers/jq-helpers.js',
     counter: './static-main/js/counter.js',
   },
   output: {
